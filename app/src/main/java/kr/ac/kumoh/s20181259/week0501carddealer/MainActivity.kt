a1b040733443e45a47c6c6e4f11c5d17768759e8
hangbo99 <102962030+hangbo99@users.noreply.github.com>
2022-10-11 15:50:18 +0900

    22.10.11 update

--- app/src/main/java/kr/ac/kumoh/s20181259/week0501carddealer/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20181259/week0501carddealer/MainActivity.kt
@@ -3,20 +3,27 @@ package kr.ac.kumoh.s20181259.week0501carddealer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import kr.ac.kumoh.s20181259.week0501carddealer.databinding.ActivityMainBinding
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val model:CardViewModel by viewModels()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        //1
         //val c = getCardName(29)//29 : 하트4
         //binding.card1.setImageResource(R.drawable.c_10_of_spades)
 
-        binding.btn1.setOnClickListener{
+
+        //2
+        /*binding.btn1.setOnClickListener{
             val res = IntArray(5)
             for(i in 0 until res.size) {
                 res[i] = resources.getIdentifier(
@@ -29,7 +36,27 @@ class MainActivity : AppCompatActivity() {
             binding.card2.setImageResource(res[1])
             binding.card3.setImageResource(res[2])
             binding.card4.setImageResource(res[3])
+            binding.card5.setImageResource(res[4])*/
+
+        //3
+        model.cards.observe(this, Observer<IntArray>{
+            val res = IntArray(5)
+            for(i in 0 until res.size) {
+                res[i] = resources.getIdentifier(
+                    getCardName(it[i]),
+                    "drawable",
+                    packageName
+                )
+            }
+            binding.card1.setImageResource(res[0])
+            binding.card2.setImageResource(res[1])
+            binding.card3.setImageResource(res[2])
+            binding.card4.setImageResource(res[3])
             binding.card5.setImageResource(res[4])
+        })
+
+        binding.btn1.setOnClickListener{
+            model.deal()
         }
     }
 
